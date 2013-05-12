3fd0a51245b2bd6b8f34cd6d0c03872d21327977
zhangda89 <zhangdapeng89@gmail.com>
2013-05-12 22:54:56 +0800

    修改了author，增加了描述等
    添加了作者信息，增加了描述，修改了错误描述，修改了日期显示格式

--- Rss.php
+++ Rss.php
@@ -1,19 +1,21 @@
 <?php
 class Rss extends CController{
-   	//public
-   	var $rss_ver = "2.0";
-   	var $channel_title = '';
-   	var $channel_link = '';
-   	var $channel_description = '';
-   	var $language = 'zh_CN';
-   	var $copyright = '';
-   	var $webMaster = '';
-   	var $pubDate = '';
-   	var $lastBuildDate = '';
-   	var $generator = 'GoWhich RSS Generator';
-   	var $content = '';
-   	var $items = array();
+    //public
+    var $rss_ver = "2.0";
+    var $channel_title = '';
+    var $channel_link = '';
+    var $channel_description = '';
+    var $language = 'zh_CN';
+    var $copyright = '';
+    var $webMaster = '';
+    var $pubDate = '';
+    var $lastBuildDate = '';
+    var $generator = 'GoWhich RSS Generator';
+    var $content = '';
+    var $author = '';
+    var $items = array();
 
+<<<<<<< HEAD
    	/**
      	* 添加基本信息
 	* @param string $title
@@ -37,125 +39,175 @@ class Rss extends CController{
      	*/
    	public function addItem($title, $link, $description ,$pubDate) {
        	$this->items[] = array('title' => $title ,
-                        'link' => $link,
-                        'descrīption' => $description,
-                        'pubDate' => $pubDate);
-   	}
+=======
+    /**
+     * 添加基本信息
+     * @param string $title
+     * @param string $link
+     * @param string $description
+     */
+    public function __construct($title, $link, $description) {
+        $this->channel_title = $title;
+        $this->channel_link = $link;
+        $this->channel_description = $description;
+        $this->pubDate = Date('r',time());
+        $this->lastBuildDate = Date('r',time());
+    }
 
+    /**
+     * 添加一个节点
+     * @param string $title
+     * @param string $link
+     * @param string $description
+     * @param date $pubDate
+     * @param string $guid
+     */
+    public function addItem($title, $link, $content ,$pubDate,$guid='') {
+        $this->items[] = array('title' => $title ,
+                'guid'=>$guid,
+>>>>>>> 修改了author，增加了描述等
+                        'link' => $link,
+                        'content' => $content,
+                        'pubDate' => $pubDate);
+    }
+
+<<<<<<< HEAD
    	/**
      	* 构建xml元素
      	*/
 	public function buildRSS() {
        	$s = <<<RSS
+=======
+    /**
+     * 构建xml元素
+     */
+  public function buildRSS() {
+        $s = <<<RSS
+>>>>>>> 修改了author，增加了描述等
 <?xml version='1.0' encoding='utf-8'?>\n
-<rss version="2.0">\n
+<rss xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" version="2.0">\n
 RSS;
 
-       	// start channel
-       	$s .= "<channel>\n";
-       	$s .= "<title><![CDATA[{$this->channel_title}]]></title>\n";
-       	$s .= "<link><![CDATA[{$this->channel_link}]]></link>\n";
-       	$s .= "<descrīption><![CDATA[{$this->channel_description}]]></descrīption>\n";
-       	$s .= "<language>{$this->language}</language>\n";
-       	if (!empty($this->copyright)) {
-        	$s .= "<copyright><![CDATA[{$this->copyright}]]></copyright>\n";
-       	}
-       	if (!empty($this->webMaster)) {
-      		$s .= "<webMaster><![CDATA[{$this->webMaster}]]></webMaster>\n";
-       	}
-       	if (!empty($this->pubDate)) {
-       		$s .= "<pubDate>{$this->pubDate}</pubDate>\n";
-       	}
-       	if (!empty($this->lastBuildDate)) {
-          	$s .= "<lastBuildDate>{$this->lastBuildDate}</lastBuildDate>\n";
-       	}
-       	if (!empty($this->generator)) {
-          	$s .= "<generator>{$this->generator}</generator>\n";
-       	}
-       	// start items
-       	for ($i=0;$i<count($this->items);$i++) {
-           	$s .= "<item>\n";
-           	$s .= "<title><![CDATA[{$this->items[$i]['title']}]]></title>\n";
-           	$s .= "<link><![CDATA[{$this->items[$i]['link']}]]></link>\n";
-           	$s .= "<descrīption><![CDATA[{$this->items[$i]['descrīption']}]]></descrīption>\n";
-           	$s .= "<pubDate>{$this->items[$i]['pubDate']}</pubDate>\n";
-           	$s .= "</item>\n";
-       	}
-      	// close channel
-      	$s .= "</channel>\n</rss>";
-      	$this->content = $s;
-   	}
+        // start channel
+        $s .= "<channel>\n";
+        $s .= "<title><![CDATA[{$this->channel_title}]]></title>\n";
+        $s .= "<atom:link href='{$this->channel_link}/feed' rel='self' type='application/rss+xml'/>\n";
+        $s .= "<link><![CDATA[{$this->channel_link}]]></link>\n";
+        $s .= "<description><![CDATA[{$this->channel_description}]]></description>\n";
+        $s .= "<language>{$this->language}</language>\n";
+        $s .= "<sy:updatePeriod>hourly</sy:updatePeriod>\n";
+        $s .= "<sy:updateFrequency>1</sy:updateFrequency>\n";
+        if (!empty($this->copyright)) {
+          $s .= "<copyright><![CDATA[{$this->copyright}]]></copyright>\n";
+        }
+        if (!empty($this->webMaster)) {
+          $s .= "<webMaster><![CDATA[{$this->webMaster}]]></webMaster>\n";
+        }
+        if (!empty($this->pubDate)) {
+          $s .= "<pubDate>{$this->pubDate}</pubDate>\n";
+        }
+        if (!empty($this->lastBuildDate)) {
+            $s .= "<lastBuildDate>{$this->lastBuildDate}</lastBuildDate>\n";
+        }
+        if (!empty($this->generator)) {
+            $s .= "<generator>{$this->generator}</generator>\n";
+        }
 
-	/**
-	 * 输出rss内容
-	 */
-	function show() {
-    	if (empty($this->content)) {
-    		$this->buildRSS();
-    	}
-       	return $this->content;
-	}
+        // start items
+        for ($i=0;$i<count($this->items);$i++) {
+          $description = Helper::truncate_utf8_string($this->items[$i]['content'], 200,'');
+          $content = $this->items[$i]['content'];
+            $s .= "<item>\n";
+            $s .= "<title><![CDATA[{$this->items[$i]['title']}]]></title>\n";
+            $s .= "<link><![CDATA[{$this->items[$i]['link']}]]></link>\n";
+            if(!empty($this->author)){
+              $s .= "<dc:creator>{$this->author}</dc:creator>\n";
+            }
+            $s .= "<description><![CDATA[{$description}]]></description>\n";
+            $s .= "<content:encoded><![CDATA[{$content}]]></content:encoded>\n";
+            $s .= "<pubDate>{$this->items[$i]['pubDate']}</pubDate>\n";
+            $s .= "<guid isPermaLink='false'>{$this->items[$i]['guid']}</guid>\n";
+            $s .= "</item>\n";
+        }
+        // close channel
+        $s .= "</channel>\n</rss>";
+        $this->content = $s;
+    }
 
-	/**
-	 * 设置版权
-	 * @param unknown $copyright
-	 */
-	public function setCopyRight($copyright){
-		$this->copyright = $copyright;
-	}
+  /**
+   * 输出rss内容
+   */
+  function show() {
+      if (empty($this->content)) {
+        $this->buildRSS();
+      }
+        return $this->content;
+  }
 
-	/**
-	 * 设置管理员
-	 * @param unknown $master
-	 */
-	public function setWebMaster($master){
-		$this->webMaster = $master;
-	}
+  function setAuthor($author){
+    $this->author = $author;
+  }
 
-	/**
-	 * 设置发布时间
-	 * @param date $date
-	 */
-	public function setpubDate($date){
-		$this->pubDate = $date;
-	}
+  /**
+   * 设置版权
+   * @param unknown $copyright
+   */
+  public function setCopyRight($copyright){
+    $this->copyright = $copyright;
+  }
 
-	/**
-	 * 设置建立时间
-	 * @param unknown $date
-	 */
-	public function setLastBuildDate($date){
-		$this->lastBuildDate = $date;
-	}
+  /**
+   * 设置管理员
+   * @param unknown $master
+   */
+  public function setWebMaster($master){
+    $this->webMaster = $master;
+  }
 
-	/**
-	 * 将rss保存为文件
-	 * @param String $fname
-	 * @return boolean
-	 */
-   	function saveToFile($fname) {
-       	$handle = fopen($fname, 'wb');
-       	if ($handle === false){
-       		return false;
-       	}
-       	fwrite($handle, $this->content);
-       	fclose($handle);
-   	}
+  /**
+   * 设置发布时间
+   * @param date $date
+   */
+  public function setpubDate($date){
+    $this->pubDate = $date;
+  }
 
-   	/**
-   	 * 获取文件的内容
-   	 * @param String $fname
-   	 * @return boolean
-   	 */
-   	function getFile($fname) {
-       	$handle = fopen($fname, 'r');
-       	if ($handle === false){
-       		return false;
-       	}
-    	while(!feof($handle)){
+  /**
+   * 设置建立时间
+   * @param unknown $date
+   */
+  public function setLastBuildDate($date){
+    $this->lastBuildDate = $date;
+  }
+
+  /**
+   * 将rss保存为文件
+   * @param String $fname
+   * @return boolean
+   */
+    function saveToFile($fname) {
+        $handle = fopen($fname, 'wb');
+        if ($handle === false){
+          return false;
+        }
+        fwrite($handle, $this->content);
+        fclose($handle);
+    }
+
+    /**
+     * 获取文件的内容
+     * @param String $fname
+     * @return boolean
+     */
+    function getFile($fname) {
+        $handle = fopen($fname, 'r');
+        if ($handle === false){
+          return false;
+        }
+      while(!feof($handle)){
             echo fgets($handle);
-    	}
-       	fclose($handle);
-   	}
+      }
+        fclose($handle);
+    }
 }
 ?>
